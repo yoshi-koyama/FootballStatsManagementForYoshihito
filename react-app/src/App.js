686be697ded795c377e1712f65a8fffcb3bf2516
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-09-24 12:21:42 +0900

    Reactの自動実行まで

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CountriesPage from './pages/CountriesPage.js';
+// import LeaguesPage from './pages/LeaguesPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CountriesPage />} />
+        {/* 国IDに基づいてリーグページに遷移 */}
+        {/* <Route path="/countries/:countryId/leagues" element={<LeaguesPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
