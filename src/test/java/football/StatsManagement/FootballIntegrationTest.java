ae88ac33ab82a8200792e580147e3f2c94cdd39b
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-10-20 18:22:10 +0900

    @Transactional追加

--- src/test/java/football/StatsManagement/FootballIntegrationTest.java
+++ src/test/java/football/StatsManagement/FootballIntegrationTest.java
@@ -20,6 +20,7 @@ import football.StatsManagement.model.domain.Standing;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -28,13 +29,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 // RepositoryTestで用いたH2データベースを利用する
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+// メソッドごとにDBの状態を元に戻す
+@Transactional
 class FootballIntegrationTest {
 
   @Autowired
@@ -43,6 +48,8 @@ class FootballIntegrationTest {
   @Autowired
   private ObjectMapper objectMapper;
 
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
   @Test
   @DisplayName("現在シーズンが取得できること")
