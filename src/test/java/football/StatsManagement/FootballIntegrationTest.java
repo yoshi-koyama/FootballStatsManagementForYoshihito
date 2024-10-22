10959aa3b773d7d3b2ebdd9eb81ef1a8e963dd8f
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-10-22 11:30:18 +0900

    リファクタリング：json比較メソッドのutilsへの移動

--- src/test/java/football/StatsManagement/FootballIntegrationTest.java
+++ src/test/java/football/StatsManagement/FootballIntegrationTest.java
@@ -22,6 +22,7 @@ import football.StatsManagement.model.domain.Standing;
 import football.StatsManagement.model.domain.json.GameResultForJson;
 import football.StatsManagement.model.domain.json.GameResultWithPlayerStatsForJson;
 import football.StatsManagement.model.domain.json.PlayerGameStatForJson;
+import football.StatsManagement.utils.TestUtils;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -643,38 +644,7 @@ class FootballIntegrationTest {
         .andReturn();
 
     String actualJson = result.getResponse().getContentAsString();
-    compareJson(expectedJson, actualJson);
-
-  }
-
-  public void compareJson(String expectedJson, String actualJson) throws Exception {
-    ObjectMapper objectMapper = new ObjectMapper();
-
-    JsonNode expectedNode = objectMapper.readTree(expectedJson);
-    JsonNode actualNode = objectMapper.readTree(actualJson);
-
-    // ここで比較ロジックを実装
-    if (!expectedNode.equals(actualNode)) {
-      // 異なる場合の処理
-      System.out.println("JSONs are not identical. Differences:");
-      logDifferences(expectedNode, actualNode);
-      // 例外をスローしてテストを失敗させる
-      throw new AssertionError("Expected and actual JSON do not match.");
-    } else {
-      System.out.println("JSONs are semantically identical.");
-    }
-  }
-
-  // 差異をログに出力するメソッド
-  private void logDifferences(JsonNode expectedNode, JsonNode actualNode) {
-    expectedNode.fieldNames().forEachRemaining(field -> {
-      if (!actualNode.has(field)) {
-        System.out.println("Missing field in actual JSON: " + field);
-      } else if (!expectedNode.get(field).equals(actualNode.get(field))) {
-        System.out.println("Field '" + field + "' differs. Expected: "
-            + expectedNode.get(field) + ", Actual: " + actualNode.get(field));
-      }
-    });
+    TestUtils.compareJson(expectedJson, actualJson);
   }
 
   @ParameterizedTest
